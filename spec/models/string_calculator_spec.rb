0910bae685384a53af59c7980c2fdf4ea0843471
javedoct24 <javedoct24@gmail.com>
2025-02-08 21:35:06 +0530

    When we pass a single number to calculator

--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 RSpec.describe StringCalculator do
-  describe "passing empty string" do
+  describe "add number calculator" do
+
     it "returns 0 for an empty string" do
       expect(StringCalculator.add("")).to eq(0)
     end
+
+    it "returns the number itself for a single number" do
+      expect(StringCalculator.add("1")).to eq(1)
+    end
+
   end
 end
