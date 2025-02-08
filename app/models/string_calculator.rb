760fa866c8683a70f7598beb78e68fb6c15e0c1f
javedoct24 <javedoct24@gmail.com>
2025-02-08 21:53:01 +0530

    When we passing the number with delimeter and new line

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -3,5 +3,6 @@ class StringCalculator
     return 0 if numbers.empty?
     return numbers.to_i if numbers.to_i.to_s == numbers
     numbers.split(",").map(&:to_i).sum
+    numbers.gsub("\n", ",").split(",").map(&:to_i).sum
   end
 end
--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -18,5 +18,9 @@ RSpec.describe StringCalculator do
       expect(StringCalculator.add("1,2,3,4")).to eq(10)
     end
 
+    it "supports new lines as delimiters" do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
+
   end
 end
