381e656c4374a5fc60784d3bf4b228c7f85940a0
javedoct24 <javedoct24@gmail.com>
2025-02-08 21:39:16 +0530

    When we pass two number into the calculator

--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -10,5 +10,9 @@ RSpec.describe StringCalculator do
       expect(StringCalculator.add("1")).to eq(1)
     end
 
+    it "returns the sum of two numbers" do
+      expect(StringCalculator.add("1,2")).to eq(3)
+    end
+
   end
 end
