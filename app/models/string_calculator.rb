947a5e7561d8f6df3d693f28c8f487e5ef66db5b
javedoct24 <javedoct24@gmail.com>
2025-02-08 22:00:16 +0530

    When we passing custom delimeter

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -4,5 +4,11 @@ class StringCalculator
     return numbers.to_i if numbers.to_i.to_s == numbers
     numbers.split(",").map(&:to_i).sum
     numbers.gsub("\n", ",").split(",").map(&:to_i).sum
+    delimiter = ","
+    if numbers.start_with?("//")
+      delimiter, numbers = numbers[2..].split("\n", 2)
+    end
+    numbers.gsub("\n", delimiter).split(delimiter).map(&:to_i).sum
+
   end
 end
